04124290ad09f17a477abc05336b971ec19c24a2
Aleksei Piianin <piyanin@gmail.com>
2021-08-20 16:38:43 +0200

    go through the row

--- src/main.rs
+++ src/main.rs
@@ -13,5 +13,22 @@ fn main() {
     let board: Vec<Vec<bool>> = BufReader::new(stdin()).lines().
         map(|l| l.unwrap().chars().into_iter().map(|c| c=='*').collect() ).collect();
 
-    println!("{:?}", board)
+    for (row_index, row) in board.iter().enumerate() {
+        for (column_index, &cell) in row.iter().enumerate() {
+            if !cell {
+                continue
+            }
+
+            // check row
+            let valid_row = {
+                let one_queen = false;
+
+                for c in 0..row.len() {
+                    if c {}
+                }
+
+                true
+            };
+        }
+    }
 }
